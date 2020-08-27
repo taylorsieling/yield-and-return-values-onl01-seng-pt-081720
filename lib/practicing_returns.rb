dde35330361ee7abc7484f1c5f925bf70103d2b9
Taylor Sieling <taylorasieling@gmail.com>
2020-08-27 19:33:33 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,10 +4,18 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
+<<<<<<< HEAD
   collection << yield(array[i])
+=======
+    collection << yield(array[i])
+>>>>>>> e6d3129c38700c082bfa0dccee295e0c1f6df5a0
     i += 1
   end
   collection
 end
 
+<<<<<<< HEAD
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+=======
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+>>>>>>> e6d3129c38700c082bfa0dccee295e0c1f6df5a0
